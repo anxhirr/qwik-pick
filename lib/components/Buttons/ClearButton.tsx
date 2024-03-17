4df0043e9a86178856a063c971c7516cfe69e331
anxhi <anxhirr@gmail.com>
2024-03-17 20:04:13 +0100

    refactor: clear icon

--- lib/components/Buttons/ClearButton.tsx
+++ lib/components/Buttons/ClearButton.tsx
@@ -2,6 +2,7 @@ import { component$, useContext } from "@builder.io/qwik"
 import styles from "./styles.module.css"
 import { InputContext } from "../../store/input"
 import { OptionsContext } from "../../store/options"
+import { ClearIcon } from "../Icons/ClearIcon"
 
 export const ClearButton = component$<{}>(() => {
   const {
@@ -20,20 +21,7 @@ export const ClearButton = component$<{}>(() => {
           .then(focusInput)
       }
     >
-      <svg
-        width="100%"
-        height="100%"
-        viewBox="-2 -2 50 50"
-        focusable="false"
-        aria-hidden="true"
-        role="presentation"
-      >
-        <path
-          fill="currentColor"
-          d="M34.923,37.251L24,26.328L13.077,37.251L9.436,33.61l10.923-10.923L9.436,11.765l3.641-3.641L24,19.047L34.923,8.124
-  l3.641,3.641L27.641,22.688L38.564,33.61L34.923,37.251z"
-        />
-      </svg>
+      <ClearIcon />
     </div>
   )
 })
